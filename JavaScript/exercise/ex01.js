5807b17004653d4480478886021023aa7676d6b6
tabasco-kimchi <cswy@naver.com>
2019-07-03 11:55:10 +0900

    js var assign

---
+++ JavaScript/exercise/ex01.js
@@ -0,0 +1,14 @@
+var name = hong 
+var age = 30
+
+console.log(name);
+console.log(age);
+
+var User = {};
+User["age"] = 20;
+User["name"] = 'ade';
+User.phonenumber = '82-10-1111-1234';
+
+console.log("name : %s", User.name);
+console.log("age : %d," User.age);
+console.log("phonenum : %s", User.phonenumber);
