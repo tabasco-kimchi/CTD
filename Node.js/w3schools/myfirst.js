4d58142a08fc9982dd14ff11346070b8ba065017
tabasco-kimchi <cswy@naver.com>
2019-07-15 17:25:49 +0900

    js first file

--- Node.js/w3schools/myfirst.js
+++ Node.js/w3schools/myfirst.js
@@ -4,3 +4,11 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World');
 }).listen(8080);
+
+<p>
+The code tells the computer to write "Hello World!" if anyone (e.g. a web browser) tries to access your computer on port 8080.
+</p>
+
+<p>
+Node.js files must be initiated in the "Command Line Interface" program of your computer.
+</p>
